765efe3fe2a45f85fca0b1b485e4668995065838
Bo-Yuan Huang <byhuang1992@gmail.com>
2018-11-27 17:52:46 -0500

    Create simple project using ilasynth lib

---
+++ example/simple/src/lib.cc
@@ -0,0 +1,15 @@
+// lib.cc
+// Synopsis: implementation of the library
+
+#include <simple/lib.h>
+#include <ilasynth/abstraction.hpp>
+#include <iostream>
+
+void Foo() {
+  //auto m = ila::Ila("bar");
+
+  //std::cout << "Hello from " << m << " in the lib." << std::endl;
+  std::cout << "Hello from the lib." << std::endl;
+
+  return;
+}
